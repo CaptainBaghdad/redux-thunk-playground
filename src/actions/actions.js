fc0ef187a9954de9534615caa8c013e63196936b
me <juniorjr083@gmail.com>
2019-07-14 11:45:54 -0400

    edit email

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,7 @@
 export const SHOW_NAME = 'SHOW_NAME';
 export const SHOW_EMAIL = 'SHOW_EMAIL';
 export const EDIT_NAME = 'EDIT_NAME';
+export const EDIT_EMAIL = 'EDIT_EMAIL';
 
 export function showIt() {
     return {type: SHOW_NAME}
@@ -15,4 +16,12 @@ export function editName(val){
         type: EDIT_NAME,
         payload: val
     }
+}
+
+export function editEmail(val){
+    return {
+        type: EDIT_EMAIL,
+        payload: val
+    }
+
 }
